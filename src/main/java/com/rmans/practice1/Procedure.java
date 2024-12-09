8db5bd951bf1ac2bc7af5443f52fa08ee2260c6e
rmansoff <130164615+rmansoff@users.noreply.github.com>
2024-12-09 16:55:44 +0200

    add procedure

---
+++ src/main/java/com/rmans/practice1/Procedure.java
@@ -0,0 +1,34 @@
+package com.rmans.practice1;
+
+
+public class Procedure {
+   
+    private int number;
+    private String name;
+    private String date;
+    private String responsiblePerson;
+    private String room;
+
+    
+    // Конструктор
+    public Procedure(int number, String name, String date, String responsiblePerson, String room) {
+        this.number = number;
+        this.name = name;
+        this.date = date;
+        this.responsiblePerson = responsiblePerson;
+        this.room = room;
+    }
+
+    // Геттери
+    public int getNumber() { return number; }
+    public String getName() { return name; }
+    public String getDate() { return date; }
+    public String getResponsiblePerson() { return responsiblePerson; }
+    public String getRoom() { return room; }
+
+    // Перевизначення toString для зручного відображення
+    @Override
+    public String toString() {
+        return number + ". " + name + " - " + responsiblePerson + " (Кабінет: " + room + ")";
+    }
+}
